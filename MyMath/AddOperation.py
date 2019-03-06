a2db483b51a7885773e2e6ecb61af5eb90e0d73c
011065WEN <M10721307@gemail.yuntech.edu.tw>
2019-03-06 21:30:53 +0800

    函式

---
+++ MyMath/AddOperation.py
@@ -0,0 +1,10 @@
+def add_x_and_y(x,y=4):
+    """
+    add x and y and return
+    :param x:
+    :param y:
+    :return:
+    """
+    sum0_num=x+y
+    return sum0_num
+
--- Student A.py
+++ Student A.py
@@ -1,70 +1,76 @@
-#machScores=[60,80,30,50,53,67,89,51,20]
-#l=len(machScores)
-#for i in range (0,l):
- #   print(i,machScores[i]*10)
-
-#for item in machScores:
-#    print(item)
-
-#for each
-#for item in machScores:
-#    item=item*10
-#    print(item)
-
-#list comprehension\
-##[item*10 for l in machScores]
-
-#family={'dad':'Homer','mom':'Lisa'}
-#for i,j in family.items():
-#    print(i,j)
-
-#x=(1,2,3)
-#y=(4,5,6)
-#z=(7,8,9)
-#for x,y,z in zip(x,y,z):
-#   print(x,y,z)
-
-##tuple1=('a','b')
-##for a,b in family.items():
-##    print(a,b)
-
-#machScores=[60,80,30,50,53,67,89,51,20]
-#l=len(machScores)
-#for i in range (0,l):
-#   print(i,machScores[i]*10)
-#ls=list(enumerate(machScores))
-#print(ls)
-#for index,item in enumerate(machScores):
-#    print(index,item)
-
-#當條件滿⾜足，則迴圈⼀一直執⾏行行
-#count=0
-#while count<10:
-#    print(count)
-#    count+=1
-
-#當條件不滿⾜足，則執⾏行行else
-#count=0
-#while count<10:
-#    print(count)
-##else:
-#    print('count>=10')
-
-#for i in range(0,20):
-#    if i % 2 == 1:
-#        print(i)
-
-#break
-##mathScores=[60,80,30,50,53,67,89,51,20]
-##for item in mathScores:
-##    if item > 88:
-##        break
-##       print(item)
+from MyMath import AddOperation as AddOpera
+#函式基本語法 def定義
+#def function_name(parameters):
+    #do something
+#    return something
+#範例
+# def add_x_and_y(x,y=4):
+#     """
+#     add x and y and return
+#     :param x:
+#     :param y:
+#     :return:
+#     """
+#     sum0_num=x+y
+#     return sum0_num
+#定義xy位置 (不打東西可以先打pass，不然會出現紅色毛毛蟲~)
+a = 10
+b = 7
+sum0=AddOpera.add_x_and_y(a,b)
+print(sum0)
+#在上面預設y=4,就算沒有定義y也會做計算
+c=AddOpera.add_x_and_y(a)
+print(c)
+#變動數量的參數個數
 
 
-#continue
-mathScores=[60,80,30,50,53,67,89,51,20]
-for i in mathScores:
-    if i >88:
-        continue
-    print(i)
+def add_x_and_multi_y(x,*y): #第二個數字後都會被歸納為y
+    """
+    add x and multi y and return sum
+    :param x: eg. 8
+    :param y: eg. (6,9,4)
+    :return:
+    """
+    #print(y) #(45,27,39,81)
+    sum_num=x+sum(y)
+    return sum_num #return 就是教函式吐東西出來
+z=6
+p=45
+c=27
+v=39
+n=81
+sum1=add_x_and_multi_y(z,p,c,v,b)
+print(sum1)
+
+sum2=add_x_and_multi_y(z,p,c,v,n,14,23)
+print(sum2)
+
+def add_x_and_y_return_multi_value(x,y):
+    sum_num=x+y
+    avg=sum_num/2
+    return sum_num,avg
+a=4
+b=27
+sum3,avg1=add_x_and_y_return_multi_value(a,b)
+print(sum3)
+print(avg1)
+
+#匿名函式
+#function_name=lambda para1,para2:do_something
+
+#filter(判斷函式，要過濾的變數)
+ls=[3,56,74,35,65,34,6,234,51]
+def is_odd(x):
+    return x % 2 == 1 #判斷除2取餘數是否等於1
+result=filter(is_odd,ls)
+f_list=list(filter(is_odd,ls))  #還要轉成字串
+print(f_list)
+
+
+#加入lambda省略 def
+f_list=list(filter(lambda x:x % 2 ==1,ls))
+print(f_list)
+
+#模組
+#import datetime
+
